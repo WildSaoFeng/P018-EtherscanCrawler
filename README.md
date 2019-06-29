276f1e812ec96a813edde9d0d543dd3d83facff0
Feng Sao <wildsaofeng@gmail.com>
2019-06-29 17:47:47 +0800

    finished ver2.3

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # P018-EtherscanCrawler
-Still Working On. 
+
+### 零.简介
+
+> File Type: Coding Project
+>
+> Version: 2.3
+>
+> Last Edit: 2019-06-29 17:47:22
+
+项目状态：已完成
+
+功能：爬取Etherscan.io的Token评论数据，以HTTP REQUEST JSON的形式存储
+
+### 一. 如果使用代码请一定注意以下几点！！
+
+1. Etherscan用的DISQUS被墙了，如果要使用代码，请一定一定要小飞机，**全局模式**，不然请求 Request Timeout
+
+2. 请保证当前文件夹下，"JSON"文件夹已建立，不然python无法新建文件夹
+
+3. Github下载代码后请补充完依赖包，PIP3 install 一下，建议采用PyCharm安装，非常方便
+
+### 二. 原理解释
+
+Etherscan和Disqus均有较为严格的反爬机制，经过多种方案尝试，最终采用 API+修改Thread参数 的方法进行修改
+
+查看网页几百行代码，多个JS文件，最终追溯到了API，以BNB为例，其API为： https://disqus.com/api/3.0/threads/listPostsThreaded?limit=50&thread=5967110845&forum=etherscan&order=popular&cursor=1%3A0%3A0&mode=2&api_key=E8Uh5l5fHZ6gD8U3KycjAIAk46f68Zw7C6eW8WSjZvCLXebZ7p0r1yrYDrLilk2F
+
+经过多次尝试，发现不同币种修改的信息仅为Thread，在Py代码中设置完HTTP Header，加载完本地Cockie后可进行迭代爬取
+
+TOP 200 的 TOKEN 的 THREAD ID 是人工爬取的，文件保存在Google在线文档： https://docs.google.com/spreadsheets/d/1l124hgG4abUmwkh2w88Xys_BID9dI2yjsjaYAeEXnFQ/edit?usp=sharing 
--- getcoin.py
+++ getcoin.py
@@ -1,13 +1,43 @@
 from urllib.request import urlopen
 import json
 import pandas as pd
+import requests
 
 df = pd.read_excel(r'./coins.xlsx')
-for i,item in df.iterrows():
-    print(i)
+
+# file = open(r'./coins.xlsx', 'rb')
+# print(file.read().decode('utf8'))
+
+for i, item in df.iterrows():
+
+    print("*")
+    print(item['THREAD'])
+    print("*")
+
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Cookie': 'disqus_unique=6cmcu223okgo89; G_ENABLED_IDPS=google; __hstc=40641725.460571c47275a01dfd57daefaf98b181.1551885197102.1551885197102.1551885197102.1; hubspotutk=460571c47275a01dfd57daefaf98b181; _ga=GA1.2.1525015291.1551885555; intercom-id-x2byp8hg=c381ffed-d67b-40ed-9729-5c98d2ca9b58; csrftoken=TPiPcbzXJpS4hCmY8cItqhKEpfcjr6Eh; __jid=1k3l56envhjip',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Host': 'disqus.com',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+    }
+    #
+    #
+    # res = requests.get('https://disqus.com/api/3.0/threads/listPostsThreaded?limit=50&thread=%d'
+    #                    %item['THREAD']+'&forum=etherscan&order=popular&cursor=1%3A0%3A0&mode=2&api_key=E8Uh5l5fHZ6'
+    #                 'gD8U3KycjAIAk46f68Zw7C6eW8WSjZvCLXebZ7p0r1yrYDrLilk2F', headers = header)
+    # print(res)
+
     res = urlopen('https://disqus.com/api/3.0/threads/listPostsThreaded?limit=50'
-                  '&thread=%d'%item['thread']+'&forum=etherscan&order=popular&cursor=1%3A0%3A0&mode=2&api_key=E8Uh5l5fHZ6'
+                  '&thread=%d'%item['THREAD']+'&forum=etherscan&order=popular&cursor=1%3A0%3A0&mode=2&api_key=E8Uh5l5fHZ6'
                   'gD8U3KycjAIAk46f68Zw7C6eW8WSjZvCLXebZ7p0r1yrYDrLilk2F').read().decode('utf-8')
+    print(res)
+
     content = json.loads(res)
-    with open("./json/%s.json"%item['name'],'w',encoding='utf-8')as f:
+    print(content)
+
+    with open("./json/%s.json" %item['COIN'],'w+',encoding='utf-8')as f:
         json.dump(content,f)
