5f76698a76b6777bf2e40de482ec7b17eb54efaf
Feng Sao <wildsaofeng@gmail.com>
2019-06-29 16:16:49 +0800

    add files

---
+++ getcoin.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+import json
+import pandas as pd
+
+df = pd.read_excel(r'E:\json\coins.xlsx')
+for i,item in df.iterrows():
+    print(i)
+    res = urlopen('https://disqus.com/api/3.0/threads/listPostsThreaded?limit=50'
+                  '&thread=%d'%item['thread']+'&forum=etherscan&order=popular&cursor=1%3A0%3A0&mode=2&api_key=E8Uh5l5fHZ6'
+                  'gD8U3KycjAIAk46f68Zw7C6eW8WSjZvCLXebZ7p0r1yrYDrLilk2F').read().decode('utf-8')
+    content = json.loads(res)
+    with open("E:\\json\\%s.json"%item['name'],'w',encoding='utf-8')as f:
+        json.dump(content,f)
